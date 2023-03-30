fbde75d7275b2faaa9aa52b5adbe4736374e9e07
Erik Lundberg <erilu308@student.liu.se>
2023-03-30 14:36:03 +0200

    Small step for man, giant leap for man kind

--- Client/Search.js
+++ Client/Search.js
@@ -1,3 +1,4 @@
+/*
 function search(keyword) {
     $.ajax({
         url: host + '/tools/search?keyword=' + keyword,
@@ -11,12 +12,39 @@ function search(keyword) {
         }
     });
 }
-
+*/
+/*
 $('#searchButton').click(function (e) {
     e.preventDefault();
+    console.log("hej")
     var keyword = $("#searchText").val();
     console.log("keyword");
     //TODO: Hämta keyword från html.
     //search(keyword);
 });
+*/
+/*
 
+var el = document.getElementById('overlayBtn');
+if(el){
+  el.addEventListener('click', sayHello, false);
+}
+*/
+function search(){
+    console.log("Hej");
+    var keyword = $("#searchText").val();
+    console.log(keyword);
+
+    $.ajax({
+        url: host + '/tools/search?keyword=' + keyword,
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        headers: { "Authorization": "Bearer " + sessionStorage.getItem('auth') },
+        success: function () {
+            //viewTools();
+            console.log("Klart");
+            return;
+        }
+    });
+}
