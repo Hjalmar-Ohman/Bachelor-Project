cbb63d9c38704b3936a0e0c19e53aea904311158
Hjalmar Öhman <hjaoh082@student.liu.se>
2023-03-31 12:32:14 +0200

    ändrade så att search heter tools

--- Client/Tools.js
+++ Client/Tools.js
@@ -1,5 +1,46 @@
+host = window.location.protocol + '//' + location.host
 
-function klickad(){
-    alert(klickad);
+function addToolCard(tool) {
+    var cardsHolder = $('.cardsHolder');
+
+    var card = $('<div class="card toolcards"></div>');
+    var productImage = $('<div class="product-image"></div>');
+    var img = $('<img src="' + tool.image + '" alt="Tool Image" draggable="false" />');
+    var productInfo = $('<div class="product-info"></div>');
+    var h2 = $('<h2>' + tool.name + '</h2>');
+    var p = $('<p>' + tool.properties + '</p>');
+    var price = $('<div class="price">' + tool.price + ' kr/h</div>');
+    var btn = $('<div class="btn"><button class="buy-btn" for="ruta">Boka nu</button></div>');
+
+    productImage.append(img);
+    productInfo.append(h2, p, price);
+    card.append(productImage, productInfo, btn);
+    cardsHolder.append(card);
 }
 
+
+function search() {
+    var keyword = $("#searchText").val();
+
+    $.ajax({
+        url: host + '/tools/search?keyword=' + keyword,
+        type: 'GET',
+        dataType: 'json',
+        contentType: 'application/json',
+        //headers: { "Authorization": "Bearer " + sessionStorage.getItem('auth') },
+        success: function (tools) {
+
+            $.each(tools, function (i, tool) {
+                addToolCard(tool);
+            });
+        }
+    });
+}
+
+$(document).ready(function () {
+    $('#searchText').on('keypress', function (e) {
+        if (e.key === 'Enter') {
+            search();
+        }
+    });
+});
