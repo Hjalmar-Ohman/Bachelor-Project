e59534d3ac107ed013e48133280297120674a372
Hjalmar Öhman <hjaoh082@student.liu.se>
2023-03-31 11:28:15 +0200

    a' venne

--- Client/Search.js
+++ Client/Search.js
@@ -1,40 +1,24 @@
 host = window.location.protocol + '//' + location.host
 
-/*
-function search(keyword) {
-    $.ajax({
-        url: host + '/tools/search?keyword=' + keyword,
-        type: 'GET',
-        data: { 'keyword': keyword },
-        dataType: 'json',
-        success: function (data) {
-            // handle successful response from server
-            console.log(data);
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            // handle error response from server
-            console.log(errorThrown);
-        }
-    });
-}
-*/
-/*
-$('#searchButton').click(function (e) {
-    e.preventDefault();
-    console.log("hej")
-    var keyword = $("#searchText").val();
-    console.log("keyword");
-    //TODO: Hämta keyword från html.
-    //search(keyword);
-});
-*/
-/*
+function addToolCard(tool) {
+    var cardsHolder = $('.cardsHolder');
 
-var el = document.getElementById('overlayBtn');
-if(el){
-  el.addEventListener('click', sayHello, false);
+    var card = $('<div class="card toolcards"></div>');
+    var productImage = $('<div class="product-image"></div>');
+    var img = $('<img src="/Pictures/tools/bearbetning.png" alt="Tool Image" draggable="false" />');
+    var productInfo = $('<div class="product-info"></div>');
+    var h2 = $('<h2>' + tool.name + '</h2>');
+    var p = $('<p>' + tool.description + '</p>');
+    var price = $('<div class="price">' + tool.price + ' kr/h</div>');
+    var btn = $('<div class="btn"><button class="buy-btn" for="ruta">Boka nu</button></div>');
+
+    productImage.append(img);
+    productInfo.append(h2, p, price);
+    card.append(productImage, productInfo, btn);
+    cardsHolder.append(card);
 }
-*/
+
+
 function search() {
     var keyword = $("#searchText").val();
 
@@ -45,9 +29,11 @@ function search() {
         contentType: 'application/json',
         //headers: { "Authorization": "Bearer " + sessionStorage.getItem('auth') },
         success: function (tools) {
+
             $.each(tools, function (i, tool) {
-                console.log(tool);
+                addToolCard(tool);
             });
         }
     });
 }
+
