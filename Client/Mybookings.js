ba311a23d368d54a7501036db463ca621c285134
Hjalmar Öhman <hjaoh082@student.liu.se>
2023-04-04 15:49:24 +0200

    allt fixat!

--- Client/Mybookings.js
+++ Client/Mybookings.js
@@ -1,3 +1,40 @@
+host = window.location.protocol + '//' + location.host
+
+function dayIntConverter(dayint) {
+    if (dayint == 1) {
+        return "Måndag"
+    } else if (dayint == 2) {
+        return "Tisdag"
+    } else if (dayint == 3) {
+        return "Onsdag"
+    } else if (dayint == 4) {
+        return "Torsdag"
+    } else if (dayint == 5) {
+        return "Fredag"
+    } else if (dayint == 6) {
+        return "Lördag";
+    } else if (dayint == 7) {
+        return "Söndag";
+    }
+}
+function addBookingRow(booking) {
+    $.ajax({
+        url: host + '/tools/' + booking.tool_id,
+        type: 'GET',
+        datatype: 'JSON',
+        contentType: 'application/json',
+        success: function (tool) {
+            var bookingsContainer = $('.bookingsContainer')
+            var cardBody = $('<div class="cardBody"></div>');
+            var button = $('<a href="#" class="btn btn-primary avbokaknapp">Avboka</a>');
+            var name = $('<h5 class="cardTitle font">' + tool.name + '</h5>');
+            var bookingTime = $('<p class="cardText font">' + dayIntConverter(booking.day) + ' ' + booking.start_hour + ':00 - ' + booking.end_hour + ':00 </p > ');
+
+            cardBody.append(button, name, bookingTime)
+            bookingsContainer.append(cardBody);
+        }
+    });
+}
 
 function getBookings() {
     $.ajax({
@@ -6,7 +43,9 @@ function getBookings() {
         datatype: 'JSON',
         contentType: 'application/json',
         success: function (user_bookings) {
-            console.log(1);
+            $.each(user_bookings, function (i, booking) {
+                addBookingRow(booking);
+            });
         }
     });
 }
