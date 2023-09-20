095d3d266f7e395099478d8c82728cff660d638e
Hjalmar Öhman <87278958+Hjalmar-Ohman@users.noreply.github.com>
2023-09-20 17:37:00 +0800

    Update stripe_functionality.py

--- Server/stripe_functionality.py
+++ Server/stripe_functionality.py
@@ -45,30 +45,3 @@ def process_payment(price, quantity, day, week, start_h, finnish_h, tool_id, use
 
     return jsonify({"session_id": session["id"]})
 
-
-# def web_hook():
-#     print("Web_hook is called")
-#     payload = request.get_data()
-#     sig_header = request.environ.get("HTTP_STRIPE_SIGNATURE")
-#     endpoint_secret = (
-#         "whsec_328c4c96086acd8e809e0a6557c93419bb3610d12ceeb1949be08d674d487da7"
-#     )
-#     event = None
-
-#     try:
-#         event = stripe.Webhook.construct_event(payload, sig_header, endpoint_secret)
-#     except ValueError as e:
-#         # Error om payload innehåller fel typer
-#         print("invalid payload")
-#         return {}, 400
-#     except stripe.error.SignatureVerificationError as e:
-#         print("invalid signature")
-#         return {}, 400
-
-#     # hämtar "check_out eventet",
-#     if event["type"] == "checkout.session.completed":
-#         session = stripe.checkout.Session.retrieve(event["data"]["object"]["id"])
-#         line_items = stripe.checkout.Session.list_line_items(session["id"], limit=1)
-#         print(line_items["data"][0]["description"])
-
-#     return {}, 200
